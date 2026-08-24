bf1c41536787293ee3bb81430d99add2c179236a
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-08-24 17:06:35 -0600

    Split the freestanding guard off the EIGEN_HAS_C99_MATH block

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -688,8 +688,13 @@
 
 // Does the compiler support C99?
 // Need to include <cmath> to make sure _GLIBCXX_USE_C99 gets defined
+// The guard covers only the include. <cmath> is unavailable in freestanding mode,
+// but the EIGEN_HAS_C99_MATH block below must still run: MathFunctions.h reads that
+// macro from outside this region, and leaving it undefined both warns under -Wundef
+// and silently selects the floor/ceil rounding fallback over ::roundf.
 #ifndef EIGEN_FREESTANDING
 #include <cmath>
+#endif
 #ifndef EIGEN_HAS_C99_MATH
 #if EIGEN_MAX_CPP_VER>=11 && \
     ((defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901))       \
@@ -699,7 +704,6 @@
   #define EIGEN_HAS_C99_MATH 1
 #else
   #define EIGEN_HAS_C99_MATH 0
-#endif
 #endif
 #endif
 
